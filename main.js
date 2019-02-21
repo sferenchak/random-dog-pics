0a38069804fee96eda89ac7e4b8f6d34acec68e6
Steve Ferenchak <steveferenchak@gmail.com>
2019-02-21 12:46:32 -0800

    Update JavaScript and Styling

---
+++ main.js
@@ -0,0 +1,17 @@
+var btn = document.querySelector("#btn");
+var img = document.querySelector("#photo");
+
+btn.addEventListener('click', function() {
+    // make the request
+    var XHR = new XMLHttpRequest();
+
+    XHR.onreadystatechange = function() {
+        if(XHR.readyState == 4 && XHR.status == 200) {
+            let url = JSON.parse(XHR.responseText).message;
+            img.src = url;
+        }
+    }
+
+    XHR.open("GET", "https://dog.ceo/api/breeds/image/random");
+    XHR.send();
+});
